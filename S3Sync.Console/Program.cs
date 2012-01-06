99adb5fce25e760f57f284457ee7f36fd5cb2966
Philippe GARRY <philippe.garry@valtech.fr>
2012-01-06 17:59:11 +0100

    Try to decrypt file :/

--- S3Sync.Console/Program.cs
+++ S3Sync.Console/Program.cs
@@ -15,9 +15,9 @@ namespace S3Sync.Console
         private const string BucketName = "DropBoxClone42";
 
         private static string _folder;
-    
 
         private static ICryptoTransform _aesEncryptor;
+        private static ICryptoTransform _aesDecryptor;
 
         private static AmazonS3Client _amazonS3Client;
 
@@ -31,7 +31,12 @@ namespace S3Sync.Console
             string password = appConfig["CryptoKey"];
             Rfc2898DeriveBytes rfcDb = new Rfc2898DeriveBytes(password, System.Text.Encoding.UTF8.GetBytes(password));
 
-            _aesEncryptor = Rijndael.Create().CreateEncryptor(rfcDb.GetBytes(16), rfcDb.GetBytes(16));
+            Rijndael rijndael = Rijndael.Create();
+           
+            byte[] key = rfcDb.GetBytes(16);
+            byte[] iv = rfcDb.GetBytes(16);
+            _aesEncryptor = rijndael.CreateEncryptor(key, iv);
+            _aesDecryptor = rijndael.CreateDecryptor(key, iv);
 
             ListBucketsResponse listBucketsResponse = _amazonS3Client.ListBuckets();
             bool bucketIsExist = listBucketsResponse.Buckets.Any(s3Bucket => s3Bucket.BucketName == BucketName);
@@ -135,24 +140,48 @@ namespace S3Sync.Console
             GetObjectResponse getObjectResponse = _amazonS3Client.GetObject(new GetObjectRequest { BucketName = BucketName, Key = s3Object.Key });
 
             string filePath = Path.Combine(_folder, s3Object.Key);
-            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.ReadWrite))
             using (BufferedStream bufferedStream = new BufferedStream(getObjectResponse.ResponseStream))
+            using (CryptoStream cryptoStream = new CryptoStream(fileStream, _aesDecryptor, CryptoStreamMode.Write))
             {
                 byte[] buffer = new byte[0x2000];
-                int count;
-                while ((count = bufferedStream.Read(buffer, 0, buffer.Length)) > 0)
+                int bytesRead;
+                do
                 {
-                    fileStream.Write(buffer, 0, count);
-                }
+                    // read a chunck of data from the input file
+                    bytesRead = bufferedStream.Read(buffer, 0, buffer.Length);
+
+                    //decrypt it
+                    cryptoStream.Write(buffer, 0, bytesRead);
+                } while (bytesRead != 0);
+                //cryptoStream.FlushFinalBlock();
             }
 
             new FileInfo(filePath).LastWriteTime = DateTime.Parse(s3Object.LastModified);
         }
 
+        private static byte[] Transform(byte[] bytes, ICryptoTransform selectCryptoTransform)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, selectCryptoTransform, CryptoStreamMode.Write))
+                {
+                    cryptoStream.Write(bytes, 0, bytes.Length);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
         private static void Main()
         {
             Initialize();
-            GetFilesOnS3();
+            //GetFilesOnS3();
+
+            //byte[] encryptedBytes = Transform(System.Text.Encoding.UTF8.GetBytes("Hello World!"), _aesEncryptor);
+            byte[] buffer = File.ReadAllBytes(@"C:\Users\Philippe\Downloads\Test (2).txt");
+            var decryptedBytes = Transform(buffer, _aesDecryptor);
+
+            System.Console.WriteLine(System.Text.Encoding.UTF8.GetString(decryptedBytes));
 
             while (System.Console.Read() != 'q')
             {
