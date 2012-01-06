1aeada6721aa91b95b2b00da44295c8f31cb2050
Philippe GARRY <philippe.garry@valtech.fr>
2012-01-06 10:29:25 +0100

    Initialize S3Client & Create Bucket if not exist

--- S3Sync.Console/Program.cs
+++ S3Sync.Console/Program.cs
@@ -1,14 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Specialized;
+using System.Configuration;
 using System.Linq;
-using System.Text;
+using Amazon.S3;
+using Amazon.S3.Model;
 
 namespace S3Sync.Console
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        // Le nom est unique pour tous les utilisateurs !
+        private const string BucketName = "DropBoxClone42";
+
+        private static AmazonS3Client _amazonS3Client;
+
+        private static void InitializeS3Client()
         {
+            NameValueCollection appConfig = ConfigurationManager.AppSettings;
+            _amazonS3Client = new AmazonS3Client(appConfig["AWSAccessKey"], appConfig["AWSSecretKey"]);
+
+            ListBucketsResponse listBucketsResponse = _amazonS3Client.ListBuckets();
+            bool bucketIsExist = listBucketsResponse.Buckets.Any(s3Bucket => s3Bucket.BucketName == BucketName);
+
+            if (!bucketIsExist)
+            {
+                _amazonS3Client.PutBucket(new PutBucketRequest().WithBucketName(BucketName));
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            InitializeS3Client();
+
+            while (System.Console.Read() != 'q')
+            {
+            }
         }
     }
 }
